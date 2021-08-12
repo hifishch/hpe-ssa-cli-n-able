c3e0b8a803d610a233982118da8834dc4b34ca7d
hifishch <56207177+hifishch@users.noreply.github.com>
2021-08-12 23:15:22 +0200

    Revert wrong commit.

--- Check-HPE-SSA-CLI.ps1
+++ Check-HPE-SSA-CLI.ps1
@@ -1,44 +1,92 @@
-#Powershell Marvell RAID VD-Check for N-able RMM
-#Author: Andreas Walker a.walker@glaronia.ch
+#Powershell HPE SSA CLI for N-able RMM
+#Author: Andreas Walker andreas.walker@walkerit.ch
 #Licence: GNU General Public License v3.0
-#Version: 1.0.4 / 12.08.2021
+#Version: 1.0.1 / 12.08.2021
 
+#Define paremeter
 Param (
-[int]$vdid
+[int]$slot = 1,
+[int]$ld = 1,
+[bool]$ssd = $false
 )
 
-$cli = "C:\Program Files (x86)\Marvell\storage\interface\mvsetup.exe"
+$el = 0
+$clipath = 'C:\Program Files\Smart Storage Administrator\ssacli\bin\'
 
-#Check if MarvellCLI is installed.
-if (!(Test-Path $cli))
+#Check if HPE SSA CLI is installed.
+if (!(Test-Path "$clipath/ssacli.exe"))
     {
-    Write-Host 'ERROR - Missing MarvellCLI! Please Download from https://support.hpe.com/hpsc/swd/public/detail?swItemId=MTX_17d87af242a144cbb3a954352c&swEnvO'
+    Write-Host 'ERROR - Missing HPE SSA CLI! Please Download from https://support.hpe.com/hpesc/public/swd/detail?swItemId=MTX_1d00dad72f544c5db131a7a5e4&swEnvOid=2000151'
     exit 1001
     }
 
-#Get output from MarvellCLI and remove unwanted lines.
-$vars = & "$cli" "info -o vd".split() | select -skip 3 | select -SkipLast 2
+#Go to CLI Path
+cd $clipath
+$Ctrl = (.\ssacli.exe ctrl slot=$slot show) | Select -skip 1
+$CtrlName = $Ctrl | Select -first 1
+$CtrlStatus = $Ctrl | Where-Object {$_ -match "Controller Status:" } 
+$CtrlStatus = $CtrlStatus -replace "   Controller Status: ",""
 
-#Dirty solution to filter VD-ID. May requires ajustment if output of MarvellCLI has different length.
-$StartLine = $vdid * 13
-$Result = $vars | select -skip $StartLine | select -First 12
-
-#Filter Get status of VD.
-$Status = $Result | Where-Object {$_ -match "status:" } | Where-Object {$_ -notmatch "BGA status:" }
-$Status = $Status -replace "^status:\s{1,}",""
-
-#Generating messages for SolarWinds RMM
-#Comment out $Result for less verbose output.
-if ($Status -ne 'functional')
+if ($CtrlStatus -ne 'OK')
     {
-    Write-Host 'ERROR - Virtual Disk' $vdid 'has status' $Status
-    $Result
-    exit 1001
+    Write-Host "ERROR - Controller $CtrlName returned status code $CtrlStatus"
+    $el = +1
     }
-    else 
+    else
         {
-        Write-Host 'OK - Virtual Disk' $vdid 'has status'$Status
-        $Result
+        Write-Host "OK - Controller $CtrlName returned status code $CtrlStatus"
+        }
+
+$ldstatusraw = (.\ssacli.exe ctrl slot=$slot ld $ld show status) | Select -Skip 1 | Select -First 1
+$ldstatusraw = $ldstatusraw.Split(":")
+$ldstatus = $ldstatusraw | Select -Last 1 | % {$_.replace(" ","")}
+$ldname = $ldstatusraw | Select -First 1 | % {$_.replace("   ","")}
+
+if ($ldstatus -ne 'OK')
+    {
+    Write-Host "ERROR - $ldname returned status code $ldstatus"
+    $el = +1
+    }
+    else
+        {
+        Write-Host "OK - $ldname returned status code $ldstatus"
+        }
+
+if ($ssd)
+    {
+    $ssdstatus = $Ctrl = (.\ssacli.exe ctrl slot=$slot show ssdinfo) | Select -skip 2
+    [int]$errorssd = $ssdstatus | Where-Object {$_ -match "Total Solid State Drives with Wearout Status:"} | % {$_.replace("   ","")} | % {$_.Split(":")} | Select -last 1
+    [int]$totalssd = $ssdstatus | Where-Object {$_ -match "Total Solid State Drives:"} | % {$_.replace("   ","")} | % {$_.Split(":")} | Select -last 1
+    $ssdlist = $ssdstatus | Where-Object {$_ -match "physicaldrive"}
+    if ($totalssd -eq 0)
+        {
+        Write-Host "WARNING - No SSD found"
+        }
+        else
+        {
+        if ($errorssd -ne 0)
+            {
+            Write-Host "ERROR - $errorssd of $totalssd SSDs reported wearout status!"
+            $ssdlist
+            $el = +1 
+            }
+            else
+            {
+            Write-Host "OK - All $totalssd SSDs reported status OK!"
+            $ssdlist
+            }
+        }
+
+    }
+
+#Generate Exit-Codes
+
+if ($el -ne 0)
+    {
+    exit 1001
+    }
+    else
+        {
         exit 0
         }
 
